101343d7e866e6c4f1107df503cb9b99d60e9f40
Rahul Manchare <133508813+founder09@users.noreply.github.com>
2026-01-06 23:45:08 +0530

    Add files via upload
    In this i learn about the props  and their some properties also learn how the UI changes the Value

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Card from "./component/Card";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" />
+    </>
+  );
+}
+
+export default App;
